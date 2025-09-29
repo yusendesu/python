b8b4faa85c4626021b627fadb76df19345b56225
yusendesu <yusendesu@outlook.com>
2025-09-29 22:00:26 +0800

    携程调用携程！

--- basic_grammar/async.py
+++ basic_grammar/async.py
@@ -1,33 +1,17 @@
 import asyncio
-import time
+from time import time
 
-async def async_hello_world():
-    now = time.time()
-    await asyncio.sleep(1)
-    print(time.time() - now)
-    print("Hello, world!")
-    await asyncio.sleep(1)
-    print(time.time() - now)
+async def upload():
+    await asyncio.sleep(2)
+
+async def download():
+    await asyncio.sleep(2)
 
 async def main():
-    await asyncio.gather(async_hello_world(), async_hello_world(), async_hello_world())
+    await asyncio.gather(upload(),download())
 
-now = time.time()
-# run 3 async_hello_world() coroutine concurrently
-asyncio.run(main())
-
-print(f"Total time for running 3 coroutine: {time.time() - now}")
-
-def normal_hello_world():
-    now = time.time()
-    time.sleep(1)
-    print(time.time() - now)
-    print("Hello, world!")
-    time.sleep(1)
-    print(time.time() - now)
-
-now = time.time()
-normal_hello_world()
-normal_hello_world()
-normal_hello_world()
-print(f"Total time for running 3 normal function: {time.time() - now}")
+if __name__ == "__main__":
+    start = time()
+    asyncio.run(main())
+    end = time()
+    print(end - start)
