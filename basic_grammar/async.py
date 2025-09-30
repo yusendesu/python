6ae0add6b001d07c29f77fc94f6b62e0037d3412
yusendesu <yusendesu@outlook.com>
2025-09-30 22:44:45 +0800

    Update async.py

--- basic_grammar/async.py
+++ basic_grammar/async.py
@@ -1,20 +1,21 @@
 import asyncio
 from time import time
-//定义携程函数
+
+# 定义协程函数
 async def upload():
-    //Await只能调用携程函数
+    # await只能调用可等待对象（如协程）
     await asyncio.sleep(2)
 
 async def download():
     await asyncio.sleep(2)
 
 async def main():
-    //将所有事件放入循环中
-    await asyncio.gather(upload(),download())
+    # 并发执行多个协程
+    await asyncio.gather(upload(), download())
 
 if __name__ == "__main__":
     start = time()
-    /
+    # 运行事件循环
     asyncio.run(main())
     end = time()
     print(end - start)
